bebf36be1a7cabea5b52ac8d5ca99df08674e34f
ByeRobin <131899176+ByeRobin@users.noreply.github.com>
2023-08-09 17:41:11 -0300

    Create practicas1.py

---
+++ practicas1.py
@@ -0,0 +1,29 @@
+#
+
+#Ejercicio 11
+valor  = input()
+print(f"******************************\n El resultado es: {valor}\n *****************************")
+
+
+
+
+
+#ejercicio 18
+cantidadDeSegundos = input("Ingresa la cantidad de segundos: ")
+
+
+#Ejercicio 19
+
+
+
+#ejercicio 20
+
+x = input("El valor de x es: ")
+y = input("El valor de y es: ")
+z = input("El valor de z es: ")
+
+print(f"El valor de x es {x}, el valor de y es {y} y el valor de z es {z}")
+
+
+
+
