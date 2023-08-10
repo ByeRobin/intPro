f9a02cb3e4fe10de95b4f0b6b5cc0d46ddbc30c2
ByeRobin <131899176+ByeRobin@users.noreply.github.com>
2023-08-10 13:41:45 -0300

    Update practicas1.py

--- practicas1.py
+++ practicas1.py
@@ -1,29 +1,90 @@
-#
+#Ejercicio 6
+print('Mi primer programa en python')
+
+#Ejercicio 7
+print('v\n \ne \n \nr \n \nt \n \ni \n \nc \n \na \n \nl')
+
+#Ejercicio 8
+print('*****\n*   *\n*   *\n*   *\n*****')
+
+#Ejercicio 9 
+print('*\n***\n*****\n*******\n*********')
+
+#Ejercicio 10
+dato = input('Dame un dato: ')
+print(dato)
 
 #Ejercicio 11
 valor  = input()
 print(f"******************************\n El resultado es: {valor}\n *****************************")
 
+#Ejercicio 12:
 
+#ejercicio 2 
 
+#def fraccionMasUno(x,y):
+#    return x/y + 1
 
+x = float(input('x = '))
+y = float(input('y = '))
+resultado = x/y + 1
+print(resultado)
 
-#ejercicio 18
-cantidadDeSegundos = input("Ingresa la cantidad de segundos: ")
+#def fraccionSumRes(x,y):
+#    return x+y/x-y
 
+x = float(input('x = '))
+y = float(input('y = '))
+resultado = x+y/x-y
+print(resultado)
 
-#Ejercicio 19
+#def fraccionSobreFraccion(x,y,z):
+#    return (x + y/z) / (x -y/z)
 
+x = float(input('x = '))
+y = float(input('y = '))
+z = float(input('z = '))
+resultado = (x + y/z) / (x -y/z)
+print(resultado)
 
+#def corchetesYparentesis(x,y):
+#    return ((x+y)**2)**2
+
+x = float(input('x = '))
+y = float(input('y = '))
+resultado = ((x+y)**2)**2
+print(resultado)
+
+#Ejercicio 3
+x = float(input('x = '))
+y = float(input('y = '))
+resultado = x + y
+print(resultado)
+
+#Ejercicio 4
+float(input('tu valor elegido es: '))
+y = x + 1 
+print(y)
+
+#Ejercicio 5
+x = float(input('x = '))
+z = float(input('z = '))
+y = x + z /2
+print(y)
+
+#Ejercicio 13
+montoinicial = float(input('Ingresa la cantidad que deseas ingresar: '))
+meses = float(input('Ingresa el plazo de disposicion(en meses): '))
+incremento = (montoinicial * 6/100) * meses
+monto_a_devolver = montoinicial + incremento
+print(f'Al finalizar tu plazo, el incremento el total a devolver sera de: {monto_a_devolver}')
+
+#Ejercicio 14
 
 #ejercicio 20
-
 x = input("El valor de x es: ")
 y = input("El valor de y es: ")
 z = input("El valor de z es: ")
 
 print(f"El valor de x es {x}, el valor de y es {y} y el valor de z es {z}")
-
-
-
 
