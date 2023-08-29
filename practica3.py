6af44eb3e02e56db2cd0691410d7c251192fd3e3
Leo-Mendoza <131899176+ByeRobin@users.noreply.github.com>
2023-08-28 21:08:49 -0300

    Create practica3.py

---
+++ practica3.py
@@ -0,0 +1,21 @@
+print("\nAct1")
+# #a
+# for n in range(1,5+1):
+#     print(n)
+# #b
+# n = int(input("Ingresa un valor numerico: "))
+# if n > 0:
+#     for n in range(1,n+1):
+#         print(n)
+# else: print("Utiliza un numero natural")
+
+i = 1
+while i <= 5:
+    num = int(input("Ingresa el num: "))
+    i = i+1
+
+print("\nAct2")
+#a
+for n in range(1,7+1):
+    print(n)
+#b
