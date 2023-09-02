31a9df4a069db553414239416b73529dfb5094b5
Leo-Mendoza <131899176+ByeRobin@users.noreply.github.com>
2023-09-02 20:38:13 -0300

    Update practicas3.py

--- practicas3.py
+++ practicas3.py
@@ -1,21 +1,31 @@
 print("\nAct1")
-# #a
-# for n in range(1,5+1):
-#     print(n)
-# #b
-# n = int(input("Ingresa un valor numerico: "))
-# if n > 0:
-#     for n in range(1,n+1):
-#         print(n)
-# else: print("Utiliza un numero natural")
-
-i = 1
-while i <= 5:
-     i = i+1
-print(i)
-
-print("\nAct2")
 #a
-for n in range(1,7+1):
+for n in range(1,5+1):
     print(n)
 #b
+n = int(input("Ingresa un valor numerico: "))
+if n > 0:
+    for n in range(1,n+1):
+        print(n)
+else: print("Utiliza un numero natural")
+
+print("\nAct2")
+
+#a
+for i in range(4,7+1):
+    print(i)
+
+#b
+m = int(input("ingresa un numero  natural: ")) #5
+n = int(input("ingresa un numero  natural: ")) #10
+
+if m!= n and m > n:
+    for i in range(m,n+1):
+        print(i)
+
+elif m!= n and n > m:
+    for i in range(n,m + 1):
+        print(i)
+
+else: print("se ejecuto el else")
+
