b2f2de412cbf948f77dc8c61847d3cea71459eb4
Leo-Mendoza <131899176+ByeRobin@users.noreply.github.com>
2023-09-08 23:16:55 -0300

    Create practicas3\12-15.py

---
+++ practicas3\12-15.py
@@ -0,0 +1,55 @@
+print("\nAct12")
+n  = int(input("Ingresa el valor de n: "))
+if n > 0:
+    for i in range(1,n+1):
+        print(i*n)
+
+print("\nAct13")
+m  = int(input("Ingresa el valor de m: "))
+i = 0
+sum = 0
+while i < m:
+    sum = sum + 1
+    i = i + sum 
+    if i > m:
+        print(sum)
+
+print("\nAct14")
+#a 
+n = int(input("Ingresa el valor de n: "))
+if n > 0:
+    for i in range(1,n+1):
+        print(2*i)
+
+#b 
+n = int(input("Ingresa el valor de n: "))
+if n > 0:
+    for i in range(1,n+1):
+        print((2*i)-1)
+#c
+n = int(input("Ingresa el valor de n: "))
+if n > 0:
+    for i in range(1,n+1):
+        print(i**2)
+
+#d
+n = int(input("Ingresa el valor de n: "))
+if n > 0:    
+    for i in range(1,n+1):
+        print((i**3) - (i**2))
+
+#e
+n = int(input("Ingresa el valor de n: "))
+if n > 0:    
+    for i in range(1,n+ 1) :
+        print(1/(i**2))
+
+
+print("\nAct15")
+#a
+n = int(input("Ingresa el valor de n: "))
+sum = 0
+for i in range(1,n+1):
+    resultado = 2*i
+    sum = sum + resultado
+    print(sum)
