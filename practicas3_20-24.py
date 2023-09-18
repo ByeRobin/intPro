8e66da42af9e14cfc962566eeeaf342909793878
Leo-Mendoza <131899176+ByeRobin@users.noreply.github.com>
2023-09-17 21:28:28 -0300

    Create practicas3_20-24.py

---
+++ practicas3_20-24.py
@@ -0,0 +1,39 @@
+print("\nAct20")
+
+#a,b
+
+izquierda = int(input("n == "))
+derecha = int(input("m == "))
+
+stop = 0
+n = izquierda 
+left = izquierda
+contador = 0
+
+while stop < 1:
+    print(left, n+contador)
+    if n+contador < derecha:
+        contador = contador + 1
+    else:
+        contador = 0
+        left = left + 1
+    if left == derecha and n + contador == derecha:
+        stop = 1
+        print(left,n + contador)
+
+print("\nAct21")
+#a
+
+print("\Act22")
+import random
+ap = int(input("Ingresa el monto a apostar: "))
+n = int(input("Ingresa tu numero (entre 0 y 99): "))
+
+for i in range(0,1):
+    numero_de_la_suerte = random.randint(0,99)
+    print(numero_de_la_suerte)
+    if numero_de_la_suerte == n:
+        ganancias = ap*70
+        print("Tus ganancias son de $",ganancias)
+    else: 
+        print("Segui participando")
