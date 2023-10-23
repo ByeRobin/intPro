bf2378fc57a7330871b2a018c8353b3ddb0fe33e
Leo :D <131899176+Leo-Mendoza@users.noreply.github.com>
2023-10-23 20:29:20 -0300

    Update practica_en_clase.py

--- practica_en_clase.py
+++ practica_en_clase.py
@@ -20,3 +20,24 @@ for i in string:
         segunda = ""
 
 print(segunda + " " + ultima)
+
+
+print("Sistema de precios supermercado")
+
+def dondeAparece(lista,blanco):
+    for i in range(len(lista)):
+        if lista[i] == blanco:
+            return i
+    return -1
+
+codigo = int(input("Ingresa el codigo del producto: "))
+
+codigos = [32,578,49]
+productos = ["azucar","yerba","arroz"]
+precios = ["1000$","2500","800"]
+
+elemento = dondeAparece(codigos,codigo)
+if elemento == -1:
+    print("Elemento inexistente")
+else: 
+    print(codigo,productos[elemento],precios[elemento])
